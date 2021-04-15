a2aec4e5398ddf1e3f645200620458ba4e60beb7
uchidanihachi28 <uchida@nihachi.com>
2021-04-15 15:44:44 +0900

    create non material design

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,7 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget{
   @override
-  Widget build(BuildContext context) => MaterialApp(
-    title: 'Flutter Layout demo',
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter layout demo'),
-      ),
-      body: Center(
-        child: Text('Hello World'),
-      ),
-    ),
+  Widget build(BuildContext context) => Center(
+        child: Text('Hello World',textDirection: TextDirection.ltr),
   );
 }
