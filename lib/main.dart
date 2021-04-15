00d258642494451cf3b547a1be22bc88e7890c9e
uchidanihachi28 <uchida@nihachi.com>
2021-04-15 13:57:11 +0900

    cerate material design

--- lib/main.dart
+++ lib/main.dart
@@ -2,46 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget{
   @override
-  _MyAppState createState() => _MyAppState();
-   }
-
-   class _MyAppState extends State<MyApp> {
-  int _counter= 0;
-
-  @override
-   Widget build(BuildContext context)  => MaterialApp(
-    title: 'Flutter Demo',
-    theme: ThemeData(
-      primarySwatch: Colors.blue,
-    ),
+  Widget build(BuildContext context) => MaterialApp(
+    title: 'Flutter Layout demo',
     home: Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-                'You have pushed the button this many times:',
-                ),
-                Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.display1,
-                )
-          ]
-        ),
+      appBar: AppBar(
+        title: Text('Flutter layout demo'),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      body: Center(
+        child: Text('Hello World'),
       ),
     ),
   );
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 }
-
